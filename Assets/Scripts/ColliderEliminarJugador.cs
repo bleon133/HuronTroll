94368e2dd284ef0b64aa47fe957b2d10e7744e43
d3vsTech <127444582+d3vsTech@users.noreply.github.com>
2024-01-28 01:21:23 -0500

    llll

--- Assets/Scripts/ColliderEliminarJugador.cs
+++ Assets/Scripts/ColliderEliminarJugador.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ColliderEliminarJugador : MonoBehaviour
 {
     [SerializeField] private GameObject jugador;
+    [SerializeField] private GameObject gameOverCanvas; 
+    [SerializeField] private float delayBeforeRespawn = 2f;
+    private float delayCanva = 3.5f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == jugador)
         {
-            Destroy(jugador);
+            Rigidbody2D rb = jugador.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = Vector2.zero; 
+                rb.isKinematic = true; 
+            }
+
+            PersonajeMovimiento movementScript = jugador.GetComponent<PersonajeMovimiento>();
+            if (movementScript != null)
+            {
+                movementScript.enabled = false; 
+            }
+
+            Animator animator = other.GetComponent<Animator>();
+            animator.Play("Muerte");
+
+            Destroy(other.gameObject, delayBeforeRespawn);
+
+            StartCoroutine(HandleDeathAndRespawn());
         }
     }
 
+    private IEnumerator HandleDeathAndRespawn()
+    {
+        yield return new WaitForSeconds(delayCanva);
+
+        gameOverCanvas.SetActive(true);
+    }
+
+    public void RestartScene()
+    {
+        // Carga la escena actual nuevamente
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
